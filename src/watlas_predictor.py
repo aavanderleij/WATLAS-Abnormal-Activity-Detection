b1a1e50bcfb0340be679ae8cbbbec1951150bb97
avanderleij <a.a.van.der.leij@st.hanze.nl>
2024-11-04 15:03:52 +0100

    added some functions for readability
    Signed-off-by: avanderleij <a.a.van.der.leij@st.hanze.nl>

--- src/watlas_predictor.py
+++ src/watlas_predictor.py
@@ -18,28 +18,63 @@ class WatlasPredictor:
         except configparser.Error as e:
             print(sys.exit(f"could not open config file: {config_file}"))
 
-        start_time = self.config['timeframe']['start_time']
+        # get start time from config dir
+        self.start_time = self.config['timeframe']['start_time']
+        # get output dir from config file
+        self.result_dir = Path(self.config['output']['output directory'])
+        
+        loaded_model = tf.keras.models.load_model("models/disturbance_model")
 
-        result_dir = Path(self.config['output']['output directory'])
-        # add start time folder to prevent accidental overwrite
-        result_dir = result_dir / str(start_time).replace(":", "-").replace(" ", "_")
+        input_df = pd.read_csv(Path(self.result_dir) / "watlas_preprediction.csv")
 
-        self.columns_to_drop = ['TAG', 'time', 'X', 'Y', 'TIME', 'tag', 'X_raw', 'Y_raw']
+        dataframe_with_predictions = self.run_model(input_df, loaded_model)
 
-        loaded_model = tf.keras.models.load_model("models/test_model")
+        self.write_results(dataframe_with_predictions)
 
-        train_data_df = pd.read_csv(Path(result_dir) / "watlas_preprediction.csv")
+
+
+    
+    def run_model(self, input_df, loaded_model):
+        """
+        runs loaded model in input dataframe
+        Returns dataframe with predictions
+
+        Returns (pd.DataFrame):
+            dataframe with predictions
+
+        """
+
+
         # drop non parameters
-        train_data_df = train_data_df.drop(columns=self.columns_to_drop)
-        # make dataframe ready for being loaded into model
-        df = {key: value.to_numpy()[:, tf.newaxis] for key, value in train_data_df.items()}
+        predict_data_df = input_df.drop(columns=self.columns_to_drop)
+        # make dataframe ready for being loaded into model, with column name being parameter name and value as tensor
+        input_dict = {key: value.to_numpy()[:, tf.newaxis] for key, value in predict_data_df.items()}
         # predict
-        results = loaded_model.predict(df)
+        results = loaded_model.predict(input_dict)
 
-        results_dataframe = train_data_df.assign(predicted=results)
+
+
+        results_dataframe = input_df.assign(predicted=results)
+        
+        return results_dataframe
+
+    def write_results(self, results_dataframe):
+        """
+        Write WATLAS data and prediction results to a csv file
+        
+        Args:
+            results_dataframe: 
+
+        Returns:
+
+        """
+
+        
+
+        # add start time folder to prevent accidental overwrite
+        result_dir = self.result_dir / str(self.start_time).replace(":", "-").replace(" ", "_")
 
         results_dataframe.to_csv(Path(result_dir) / "predictions.csv")
-
 
 
 
